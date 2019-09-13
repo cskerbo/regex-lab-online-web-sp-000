58ca569054fb4e91efab36ee9112e84ca31236e2
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-13 19:47:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  vowel?(word) = word.scan(/\b[aeiou]\w*/i)
+  vowel?(word) = word.match(/\b[aeiou]\w*/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
