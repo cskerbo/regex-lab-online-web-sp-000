790ab473b18aa0ce190f5e79da2bdeac5df8119c
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-13 20:50:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-  vowel?(word) = word.match(/\b[aeiou]\w*/i)
+  vowels = [word.scan(/\b[aeiou]\w*/i)]
+  vowels
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
