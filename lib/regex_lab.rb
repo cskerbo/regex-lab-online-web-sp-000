8de6e2878b706b6777766d49b3e9acc7aba8ae8b
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-13 20:53:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
-  vowels = [word.scan(/\b[aeiou]\w*/i)]
-  puts vowels
+  vowels = word.scan(/\b[aeiou]\w*/i)
+  vowels != true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
